dabe444e7b1c96fdd5e47d7de3d3807a6749f595
Daniel J. Pritchett <dpritchett@gmail.com>
2015-06-26 18:38:25 -0500

    Announcing join; die when KICKed

--- main.go
+++ main.go
@@ -11,20 +11,44 @@ const (
 	VERSION = "glisten v1.0"
 )
 
-// Dump an irc message to the log
-func PrintEvent(e *irc.Event) {
-	channel := e.Arguments[0]
-	nick := e.Nick
-	msg := e.Message()
-	log.Println(channel + " <" + nick + "> " + msg)
+type Message struct {
+	Event   *irc.Event
+	Channel string
+	Nick    string
+	Message string
 }
 
+func NewMessage(e *irc.Event) *Message {
+	m := &Message{}
+	m.Event = e
+
+	m.Channel = e.Arguments[0]
+	m.Nick = e.Nick
+	m.Message = e.Message()
+
+	return m
+}
+
+func (m *Message) ToString() string {
+	return m.Channel + " <" + m.Nick + "> " + m.Message
+}
+
+// Dump an irc message to the log
+func PrintEvent(e *irc.Event) {
+	msg := NewMessage(e)
+	log.Println(msg.ToString())
+}
+
+var progArgs = []string{}
+
 func main() {
-	if len(os.Args) != 5 {
+	progArgs = os.Args[1:]
+
+	if len(progArgs) != 4 {
 		log.Fatal("Usage example: glisten \"irc.freenode.net:6667\" glisten_botnick glisten_botname \"#memtechbot\"")
 	}
 
-	network, nick, username, channel := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+	network, nick, username, channel := progArgs[0], progArgs[1], progArgs[2], progArgs[3]
 
 	// connect
 	cnxn := irc.IRC(nick, username)
@@ -42,6 +66,24 @@ func main() {
 
 	// join specified channel on successful network connect
 	cnxn.AddCallback("001", func(e *irc.Event) { cnxn.Join(channel) })
+
+	// Announce successful JOIN
+	cnxn.AddCallback("366", func(e *irc.Event) {
+		log.Println("** JOINed " + e.Arguments[1])
+	})
+
+	// Die when KICKed
+	// Arguments:[]string{"#MemphisRuby", "glisten_botnick", "test kick bot"}
+	cnxn.AddCallback("KICK", func(e *irc.Event) {
+		channel, user := e.Arguments[0], e.Arguments[1]
+		msg := "** User " + user + " was kicked from " + channel + "."
+
+		if user == progArgs[1] {
+			log.Fatal(msg)
+		} else {
+			log.Println(msg)
+		}
+	})
 
 	err := cnxn.Connect(network)
 
