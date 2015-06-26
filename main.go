4499b5ba9c511429c661294a5c087c8e4cd3a839
Daniel J. Pritchett <dpritchett@gmail.com>
2015-06-26 14:32:35 -0500

    Some comments for basic use case

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	// connect
 	irccon1 := irc.IRC("glisten-tester", "glisten-tester")
 	irccon1.VerboseCallbackHandler = true
-	irccon1.Debug = true
+	irccon1.Debug = true // pretty noisy, probably need to squelch it at some point
 
 	err := irccon1.Connect("irc.freenode.net:6667")
 
@@ -21,6 +21,7 @@ func main() {
 	// 001: Welcome to the network [https://tools.ietf.org/html/rfc2812]
 	irccon1.AddCallback("001", func(e *irc.Event) { irccon1.Join("#memtechbot") })
 
+	// Despite the name, PRIVMSG seems to pass on non-private messages too
 	irccon1.AddCallback("PRIVMSG", func(e *irc.Event) { log.Println("<" + e.Nick + "> " + e.Message()) })
 
 	irccon1.Loop()
